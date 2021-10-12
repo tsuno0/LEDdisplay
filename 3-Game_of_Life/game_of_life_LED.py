6f2e2c7674723d59baf825c4e5a1102a1d018f85
π&ϱ <59118739+tsuno0@users.noreply.github.com>
2021-10-12 10:07:14 +0200

    GoL console fonctionnel

--- 3-Game_of_Life/game_of_life_LED.py
+++ 3-Game_of_Life/game_of_life_LED.py
@@ -1,60 +1,138 @@
+# !/usr/bin/env python3
+
+import time
+import os
+import random
+import sys
 from arrayLED import *
 
-array = [[N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
-         [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N]]
 
-myLed = ArrayLED(array)
+def create_initial_grid():
+    grid = [[N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N],
+            [N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N,N]]
+    return grid
 
-def update_life():
-    next = [[0 for x in range(myLed.width)] for y in range(myLed.height)] 
-    for i in range(myLed.width):
-        for j in range(myLed.height):
-            neighbors = count_neighbors(i,j)
 
-            if (myLed.array[i][j] == 0 and neighbors == 3):
-                next[i][j] = 1
-            elif (myLed.array[i][j] == 1 and (neighbors < 2 or neighbors > 3)) :
-                next[i][j] = 0
+def print_grid(rows, cols, grid, myLED):
+    myLED.array = grid
+    myLED.update()
+
+
+def create_next_grid(rows, cols, grid, next_grid):
+    """
+    Analyzes the current generation of the Game of Life grid and determines what cells live and die in the next
+    generation of the Game of Life grid.
+    :param rows: Int - The number of rows that the Game of Life grid has
+    :param cols: Int - The number of columns that the Game of Life grid has
+    :param grid: Int[][] - The list of lists that will be used to represent the current generation Game of Life grid
+    :param next_grid: Int[][] - The list of lists that will be used to represent the next generation of the Game of Life
+    grid
+    """
+    for row in range(rows):
+        for col in range(cols):
+            # Get the number of live cells adjacent to the cell at grid[row][col]
+            live_neighbors = get_live_neighbors(row, col, rows, cols, grid)
+
+            # If the number of surrounding live cells is < 2 or > 3 then we make the cell at grid[row][col] a dead cell
+            if live_neighbors < 2 or live_neighbors > 3:
+                next_grid[row][col] = N
+            # If the number of surrounding live cells is 3 and the cell at grid[row][col] was previously dead then make
+            # the cell into a live cell
+            elif live_neighbors == 3 and grid[row][col] == N:
+                next_grid[row][col] = B
+            # If the number of surrounding live cells is 3 and the cell at grid[row][col] is alive keep it alive
             else:
-                next[i][j] = myLed.array[i][j]
-    myLed.array = next
+                next_grid[row][col] = grid[row][col]
 
-def count_neighbors(x,y):
-    sum = 0
+
+def get_live_neighbors(row, col, rows, cols, grid):
+    """
+    Counts the number of live cells surrounding a center cell at grid[row][cell].
+    :param row: Int - The row of the center cell
+    :param col: Int - The column of the center cell
+    :param rows: Int - The number of rows that the Game of Life grid has
+    :param cols: Int - The number of columns that the Game of Life grid has
+    :param grid: Int[][] - The list of lists that will be used to represent the Game of Life grid
+    :return: Int - The number of live cells surrounding the cell at grid[row][cell]
+    """
+    life_sum = 0
     for i in range(-1, 2):
         for j in range(-1, 2):
-            myLed.width = (x + i + myLed.width) % myLed.width
-            myLed.height = (y + j + myLed.height) % myLed.height
-            sum += myLed.array[myLed.width][myLed.height]
-    sum -= myLed.array[x][y]
-    return sum
+            # Make sure to count the center cell located at grid[row][col]
+            if not (i == 0 and j == 0):
+                # Using the modulo operator (%) the grid wraps around
+                life_sum += grid[((row + i) % rows)][((col + j) % cols)] == B
+    return life_sum
 
-def print_matrix():
-    for i in range(myLed.width):
-        for j in range(myLed.height):
-            print(myLed.array[i][j])
-        print()
 
-while True:
-    update_life()    
-    print_matrix()
-    # myLed.update()
+def grid_changing(rows, cols, grid, next_grid):
+    """
+    Checks to see if the current generation Game of Life grid is the same as the next generation Game of Life grid.
+    :param rows: Int - The number of rows that the Game of Life grid has
+    :param cols: Int - The number of columns that the Game of Life grid has
+    :param grid: Int[][] - The list of lists that will be used to represent the current generation Game of Life grid
+    :param next_grid: Int[][] - The list of lists that will be used to represent the next generation of the Game of Life
+    grid
+    :return: Boolean - Whether the current generation grid is the same as the next generation grid
+    """
+    for row in range(rows):
+        for col in range(cols):
+            # If the cell at grid[row][col] is not equal to next_grid[row][col]
+            if not grid[row][col] == next_grid[row][col]:
+                return True
+    return False
+
+
+def run_game():
+
+    # Get the number of rows and columns for the Game of Life grid
+    rows = 20
+    cols = 30
+
+    # Get the number of generations that the Game of Life should run for
+    generations = 5000
+
+    # Create the initial random Game of Life grids
+    current_generation = create_initial_grid(rows, cols)
+    next_generation = create_initial_grid(rows, cols)
+
+    myLED = ArrayLED(current_generation)
+
+    # Run Game of Life sequence
+    gen = 1
+    for gen in range(1, generations + 1):
+        if not grid_changing(rows, cols, current_generation, next_generation):
+            break
+        print_grid(rows, cols, current_generation, myLED)
+        create_next_grid(rows, cols, current_generation, next_generation)
+        time.sleep(1 / 5.0)
+        current_generation, next_generation = next_generation, current_generation
+
+    print_grid(rows, cols, current_generation, myLED)
+    return input("<Enter> to exit or r to run again: ")
+
+
+# Start the Game of Life
+run = "r"
+while run == "r":
+    out = run_game()
+    run = out
